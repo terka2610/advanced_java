53f219364c84e738285227e38e7db1899e816d0f
playboifufalo <krasvorp@gmail.com>
2025-08-05 15:49:19 +0200

    Improved the classes

--- game_logic/logic/Card.java
+++ game_logic/logic/Card.java
@@ -1,5 +1,7 @@
 package logic;
 
+import java.util.Arrays;
+
 public class Card {
 	private String value;
 	private String suit;
@@ -15,4 +17,13 @@ public class Card {
 	public void setSuit(String suit) {
 		this.suit = suit;
 	}
+	public int getNumericValue() {
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        int index = Arrays.asList(ranks).indexOf(value.toLowerCase());
+        return (index >= 0) ? index + 2 : 0; 
+    }
+
+    public String getImagePath() {
+        return "GUI/src/images/" + value + suit + ".png";
+    }
 }
--- game_logic/logic/DeckOfCards.java
+++ game_logic/logic/DeckOfCards.java
@@ -7,23 +7,23 @@ import java.util.List;
 public class DeckOfCards {
     private List<Card> playerDeck = new ArrayList<>();
     private List<Card> computerDeck = new ArrayList<>();
-    private String[] suits = { "clubs", "diamonds", "hearts", "spades" };
-    private String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", 
-                             "jack", "queen", "king", "ace" };
+    private String[] suits = {"Club", "Diamond", "Heart", "Spade"};
+    private String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
 
     private void shuffleDeck(List<Card> deck) {
         for (int i = deck.size() - 1; i > 0; i--) {
-            int j = (int) (Math.random() * (i + 1));		//shuffling the deck function
+            int j = (int) (Math.random() * (i + 1));
             Card temp = deck.get(i);
             deck.set(i, deck.get(j));
             deck.set(j, temp);
         }
     }
+
     private void initializeDeck() {
         playerDeck.clear();
         computerDeck.clear();
-        List<Card> fullDeck = new ArrayList<>();		//deck initialization
-        
+        List<Card> fullDeck = new ArrayList<>();
+
         for (String suit : suits) {
             for (String rank : ranks) {
                 Card card = new Card();
@@ -41,10 +41,11 @@ public class DeckOfCards {
             }
         }
     }
+
     private int compareCards(Card card1, Card card2) {
-        int rank1 = Arrays.asList(ranks).indexOf(card1.getValue());
-        int rank2 = Arrays.asList(ranks).indexOf(card2.getValue());
-        return Integer.compare(rank1, rank2);
+        return Integer.compare(card1.getNumericValue(), card2.getNumericValue());
     }
     //TODO: Add a war game class and make the methods for the game
+    // Add start a round and start a game classes
+    // Add a joker for a deck
 }
